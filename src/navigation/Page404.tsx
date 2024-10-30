e38ac58aca861795d6f4a8615b97dfd5b8a8283d
jose <jose@devtzal.com>
2024-10-30 16:41:41 -0500

    :lipstick: Implementar estilos a la página 404
    - Implementación de link a home

--- src/navigation/Page404.tsx
+++ src/navigation/Page404.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface Page404Props {}
 
 const Page404: React.FC<Page404Props> = ({}) => {
-  return <div>Page404</div>;
+  return (
+    <div className="page-404">
+      <div>
+        <h1>404</h1>
+      </div>
+      <div>
+        <h2>Página no encontrada</h2>
+      </div>
+
+      <div className="cursor-pointer">
+        <Link to="/">Ir a la página de inicio</Link>
+      </div>
+    </div>
+  );
 };
 export { Page404 };
